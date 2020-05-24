0a70c815c99ada130c710bd886c7172ada8b7303
dzintars-magons <57200665+dzintars-magons@users.noreply.github.com>
2020-05-24 19:58:01 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const terser = require('gulp-terser');
+const autoprefix = require('gulp-autoprefixer');
+const {parallel} = require('gulp');
+
+const jsPath = './src/js/**/*.js';
+const scssPath = './src/scss/**/*.scss';
+
+function copyHtml(){
+    return gulp.src('src/*.html').pipe(gulp.dest('build'));
+}
+
+
+function copyImages(){
+    return gulp.src('src/img/**/*').pipe(gulp.dest('build/img'));
+}
+
+
+//compile scss into css
+function style(){
+    
+    return gulp.src(scssPath)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+        includePaths: scssPath,
+        outputStyle: 'compressed'
+    })
+    .on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefix('last 4 versions'))
+    .pipe(gulp.dest('./build/css')) 
+    .pipe(browserSync.stream());
+}
+
+function js(){
+    return gulp.src(jsPath)
+    .pipe(sourcemaps.init())
+    .pipe(concat('all.js'))
+    .pipe(terser())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./build/js'))
+    .pipe(browserSync.stream());
+}
+
+function watch(){
+    browserSync.init({
+        server: {
+            baseDir: './build'
+        }
+    });
+    gulp.watch('./src/scss/**/*.scss', style);
+    gulp.watch('./src/*html', copyHtml).on('change', browserSync.reload);
+    gulp.watch('./src/js/**/*.js', js).on('change', browserSync.reload);
+    gulp.watch('./src/img/**/*', copyImages).on('change', browserSync.reload);
+}
+
+exports.copyHtml = copyHtml;
+exports.copyImages = copyImages;
+exports.style = style;
+
+
+exports.build = parallel(copyHtml, copyImages, style, js);
+exports.watch = watch; 
